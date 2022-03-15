dd61d05fa383b02d166981fdec8fd1053193c29b
Kate Owens <kate.owens@deque.com>
2022-03-15 16:41:09 -0400

    small changes

--- axe-devtools-ios-sample-app/axe-devtools-ios-sample-app/Views/SearchBarView.swift
+++ axe-devtools-ios-sample-app/axe-devtools-ios-sample-app/Views/SearchBarView.swift
@@ -9,22 +9,18 @@ import Foundation
 import UIKit
 
 class SearchBarView: UIView {
-// figure out how to get search field border to be white
-    lazy var titleLabel: UILabel = {
-        let l = UILabel()
-        l.text = "Chic Boutique"
-        l.textColor = .white
-        l.backgroundColor = .black
-        l.translatesAutoresizingMaskIntoConstraints = false
-        l.font = UIFont.preferredFont(forTextStyle: .title1)
-        return l
+
+    lazy var titleImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        let image = UIImage(named: "chic boutique")
+        iv.image = image
+        return iv
     }()
 
     lazy var searchBar: UISearchBar = {
         let sb = UISearchBar()
-        sb.text = "Search..."
-        sb.barStyle = UIBarStyle.black
-        sb.searchTextField.borderStyle = .roundedRect
+        sb.backgroundColor = .black
         sb.isUserInteractionEnabled = false
         sb.translatesAutoresizingMaskIntoConstraints = false
         return sb
@@ -32,6 +28,7 @@ class SearchBarView: UIView {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+        backgroundColor = .black
         buildView()
     }
 
@@ -40,26 +37,25 @@ class SearchBarView: UIView {
     }
 
     override func layoutSubviews() {
-        backgroundColor = .black
         self.layer.cornerRadius = 15
         self.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
     }
     private func buildView() {
+        self.addSubview(titleImageView)
         self.addSubview(searchBar)
-        self.addSubview(titleLabel)
         updateConstraints()
     }
 
     override func updateConstraints() {
         super.updateConstraints()
         NSLayoutConstraint.activate([
-            self.titleLabel.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
-            self.titleLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor)
-        ])
+            self.titleImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 60),
+            self.widthAnchor.constraint(equalToConstant: 161),
+            self.titleImageView.centerXAnchor.constraint(equalTo: self.centerXAnchor)
+            ])
 
         NSLayoutConstraint.activate([
-            self.titleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 16),
-            self.searchBar.topAnchor.constraint(equalTo: self.titleLabel.bottomAnchor, constant: 16),
+            self.searchBar.topAnchor.constraint(equalTo: self.titleImageView.bottomAnchor, constant: 16),
             self.searchBar.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 24),
             self.searchBar.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -24),
             self.searchBar.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -16)
