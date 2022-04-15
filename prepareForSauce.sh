8d49a41e1e42e96157d0dcde31b07f4d43d96d00
Kim Arnett <kim.arnett@deque.com>
2022-04-15 13:24:30 -0400

    working checkpoint

--- prepareForSauce.sh
+++ prepareForSauce.sh
@@ -1,25 +1,32 @@
-  APP_LOCATION="DerivedData/Build/Products/Debug-iphoneos"
-  APP_NAME="axe-devtools-ios-sample-app"
-  xcodebuild build-for-testing -configuration Release \
-           -scheme "axe-devtools-ios-sample-app" \
-           -target "axe-devtools-ios-sample-appUITests" \
-           -sdk iphoneos \
-           -derivedDataPath "./DerivedData"
-  mkdir Payload
-  mkdir apps
-  mv $APP_LOCATION/$APP_NAME.app Payload
-  zip -r "$APP_NAME.ipa" Payload
-  rm -rf Payload/*
+APP_LOCATION="DerivedData/Build/Products/Debug-iphoneos"
+APP_NAME="axe-devtools-ios-sample-app"
 
-  mv $APP_LOCATION/axe-devtools-ios-sample-appUITests-Runner.app Payload
-  zip -r "axe-devtools-ios-sample-appUITests-Runner.ipa" Payload
-  rm -rf Payload
+rm -rf *.ipa
+
+xcodebuild build-for-testing -configuration Debug \
+       -scheme "axe-devtools-ios-sample-app" \
+       -target "axe-devtools-ios-sample-appUITests" \
+       -sdk iphoneos \
+       -derivedDataPath "./DerivedData"
+
+mkdir Payload
+mkdir apps
+
+mv $APP_LOCATION/$APP_NAME.app Payload
+zip -r "apps/$APP_NAME.ipa" Payload
+rm -rf Payload/*
+
+mv $APP_LOCATION/axe-devtools-ios-sample-appUITests-Runner.app Payload
+zip -r "apps/axe-devtools-ios-sample-appUITests-Runner.ipa" Payload
+
 
 #  curl -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY.ipa" --location \
- #     --request POST 'https://api.us-west-1.saucelabs.com/v1/storage/upload' \
-  #    --form 'payload=@""' \
-   #     --form 'name=$APP_NAME'
+#     --request POST 'https://api.us-west-1.saucelabs.com/v1/storage/upload' \
+#    --form 'payload=@""' \
+#     --form 'name=$APP_NAME'
 
-  saucectl run
-  rm -rf apps
-  rm -rf DerivedData
+saucectl run
+
+#rm -rf apps
+rm -rf Payload
+rm -rf DerivedData
