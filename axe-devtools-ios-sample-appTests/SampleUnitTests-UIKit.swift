ca1fbdd686848d9a92d4855610980fd517f886a7
Kim Arnett <kim.arnett@deque.com>
2022-05-16 10:05:10 -0400

    Update axe-devtools-ios-sample-appTests/SampleUnitTests-UIKit.swift
    Co-authored-by: Jennifer Korth <jennifer.dailey@deque.com>

--- axe-devtools-ios-sample-appTests/SampleUnitTests-UIKit.swift
+++ axe-devtools-ios-sample-appTests/SampleUnitTests-UIKit.swift
@@ -24,7 +24,7 @@ class SampleUnitTests: XCTestCase {
             return
         }
 
-        let _ = try? axe?.saveResult(result)
+        let _ = try axe?.saveResult(result)
         XCTAssertEqual(result.failures.count, 0)
     }
 }
