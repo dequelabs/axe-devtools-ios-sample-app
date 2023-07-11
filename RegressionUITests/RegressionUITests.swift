6fb0367ed6d654f9139d6937e52258b35d9a90e6
Kim Arnett <kim.arnett@deque.com>
2023-07-11 09:59:25 -0400

    add regression scheme and reporter github action

---
+++ RegressionUITests/RegressionUITests.swift
@@ -0,0 +1,44 @@
+//
+//  RegressionUITests.swift
+//  RegressionUITests
+//
+
+import axe_devtools_ios_sample_app
+import axeDevToolsXCUI
+import XCTest
+
+class RegressionUITests: XCTestCase {
+    var axe: AxeDevTools?
+    var app = XCUIApplication()
+
+    override func setUp() {
+        axe = try? AxeDevTools.login(withAPIKey: Login.APIKey)
+
+        app.launch()
+        sleep(1)
+    }
+
+    // Iterates through each tab of the sample application and runs an accessibility scan on the screen, then posts it to the dashboard. Contains a few different options for implementing -- feel free to play around with it!
+    func testHappyPathAccessibility() throws {
+        // Run a scan on the first page and post the result to the dashboard.
+        try scanForAccessibility()
+
+        //navigate to a tab by it's index, and run a scan, post to the dashboard
+        let tabBar = XCUIApplication().tabBars["Tab Bar"]
+        tabBar.children(matching: .button).element(boundBy: 1).tap()
+        try scanForAccessibility()
+
+        //navigate to a tab by its title
+        tabBar.buttons["Cart"].tap()
+        try scanForAccessibility()
+
+        tabBar.children(matching: .button).element(boundBy: 3).tap()
+        try scanForAccessibility()
+    }
+
+    // A helper method for keeping things cleaner when pushing to the dashboard, or saving a result locally.
+    func scanForAccessibility() throws {
+        guard let result = try axe?.run(onElement: app) else { XCTFail(); return }
+        try axe?.saveResult(result, toPath: "RegressionScans")
+    }
+}
