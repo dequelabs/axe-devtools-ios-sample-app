9fee0900995982ea0d20404788038808531b4577
Kate Owens <kate.owens@deque.com>
2023-10-11 10:58:52 -0400

    updated formatting and edited words a little bit

--- axe-devtools-ios-sample-appUITests/SampleUITests-XCUI.swift
+++ axe-devtools-ios-sample-appUITests/SampleUITests-XCUI.swift
@@ -9,53 +9,94 @@ import axe_devtools_ios_sample_app
 import axeDevToolsXCUI
 import XCTest
 
-// For more information on automated testing, please visit: https://docs.deque.com/devtools-mobile/2023.8.16/en/xcui
+/*
+ Full Getting Started guide is available here: https://docs.deque.com/devtools-mobile-analyzer/2023.8.16/en/ios-setup
+
+ Quick Start Guide
+
+ To get started:
+ Set up axeDevTools automation for UITests in the 2 easy steps outlined below.
+ Don't forget to save this file when finished. File > Save or Command + S
+
+ To run an accessibility test:
+ Select a simulator or device in Xcode and press cmd+U to begin running tests.
+ Review your test results from the Mobile Dashboard: https://axe-mobile.deque.com/
+ */
+
 class SampleUITestsXCUIFramework: XCTestCase {
     var axe: AxeDevTools?
     var app = XCUIApplication()
 
     override func setUp() {
+        // 1.
+        // Enter your axe DevTools Mobile API key in the "" marks in the APIKey constant defined in the Login file
+        // Get your API key here: https://axe.deque.com/settings
         axe = try? AxeDevTools.login(withAPIKey: Login.APIKey)
+
+        // 2.
+        // Launch the sample app
         app.launch()
         sleep(1)
     }
 
-    // This test runs an accessibility scan on each tab in the sample application, then posts to the dashboard. Contains a few different options for implementing -- feel free to play around with it!
+    /*
+     This test runs an accessibility scan on each tab in the sample application, then posts to the dashboard.
+     This example contains a few different options for implementing -- feel free to play around with it!
+     */
     func testAccessibilityAndPostResults() throws {
-        guard let result = try axe?.run(onElement: app) else { return XCTFail() } 
-        // Run a scan on the first page of the sample app
-        // For more information about running an accessibility scan visit: https://docs.deque.com/devtools-mobile/2023.8.16/en/xcui#capture-a-scan
 
-        try axe?.postResult(result)
+        // Run a scan on the first page of the sample app
+        // For more information about running an accessibility scan visit:
+        // https://docs.deque.com/devtools-mobile/2023.8.16/en/xcui#capture-a-scan
+        guard let result = try axe?.run(onElement: app) else { return XCTFail() }
+
         // Post scan results to the mobile dashboard
-        // For more information on this feature please visit:  https://docs.deque.com/devtools-mobile/2023.8.16/en/xcui#send-results-to-the-dashboard
+        // For more information on this feature please visit:
+        // https://docs.deque.com/devtools-mobile/2023.8.16/en/xcui#send-results-to-the-dashboard
+        try axe?.postResult(result)
 
         let tabBar = XCUIApplication().tabBars["Tab Bar"]
 
         tabBar.buttons["Catalog"].tap()
-        guard let result = try axe?.run(onElement: app) else { return XCTFail() } 
-        try axe?.postResult(result, withScanName: "Catalog")
-        // Rename a scan and post it to the dashboard. For more information on renaming a scan, check out  https://docs.deque.com/devtools-mobile/2023.8.16/en/ios-scan-name
 
-        tabBar.buttons["Cart"].tap() // Navigate to a tab by its title
         guard let result = try axe?.run(onElement: app) else { return XCTFail() }
-        try axe?.postResult(result, withTags: ["Cart Tab", "iPhone 15 Pro"]) // Add tags to your scan to easily share with your coworkers when it's pushed to the dashboard.
-        // For more infromation on tagging a scan please see: https://docs.deque.com/devtools-mobile/2023.8.16/en/ios-tag-result
+
+        // Add a custom scan name when posting result. By default, the screen title is the scan name.
+        // For more information on renaming a scan, check out:
+        // https://docs.deque.com/devtools-mobile/2023.8.16/en/ios-scan-name
+        let newScanName = "Catalog Tab"
+
+        try axe?.postResult(result, withScanName: newScanName)
+
+        tabBar.buttons["Cart"].tap()
+        guard let result = try axe?.run(onElement: app) else { return XCTFail() }
+
+        // Add tags to your scan to easily share with your coworkers when it's pushed to the dashboard.
+        // For more infromation on tagging a scan please see:
+        // https://docs.deque.com/devtools-mobile/2023.8.16/en/ios-tag-result
+        let tags = ["Cart Tab, iPhone 15 Pro"]
+
+        try axe?.postResult(result, withTags: tags)
 
         tabBar.buttons["Profile"].tap()
         guard let result = try axe?.run(onElement: app) else { return XCTFail() }
         XCTAssertEqual(result.failures.count, 0) // Fail the test if any accessibilitity issues are found in the scan
     }
 
-    // This test runs an accessibility scan on the first page of the sample application and saves the scan results locally.
+    // This test runs an accessibility scan on the first page of the sample application and saves the scan results locally. By default, the results will be saved to a folder called 'AxeDevToolsResults' within your local User folder
+    // For more information on saving results locally, please visit:
+    // https://docs.deque.com/devtools-mobile/2023.8.16/en/ios-save-result
     func testAccessibilityAndSaveResultsLocally() throws {
         var lastResult: AxeResult?
-
         guard let result = try axe?.run(onElement: app) else { return XCTFail() }
-        let savedScan = try axe?.saveResult(result, withScanName: "Sample App Accessibility scan") // Save the results of the accessibility scan; by default, they will be saved to a folder called 'AxeDevToolsResults' within your local User folder
-        // for more information on saving results locally see: https://docs.deque.com/devtools-mobile/2023.8.16/en/ios-save-result
+
+        // Save scan results locally
+        try axe?.saveResult(result)
+
         lastResult = result
         let critical = lastResult?.failures.filter { $0.impact == .CRITICAL }.count
-        XCTAssertTrue(critical == 0, "Critical Accessibility Results were found." ) // Assert that there are no critical impact accessibility issues in scan results
+
+        // Assert that there are no critical impact accessibility issues in scan results
+        XCTAssertTrue(critical == 0, "Critical Accessibility Results were found." )
     }
 }
