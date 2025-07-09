ced025daee8c800910667d88ecc9dc99247001f4
Kate Owens <kate.owens@deque.com>
2025-07-09 14:32:07 -0400

    wake up xcode cloud

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -8,3 +8,12 @@
 
 # This would be a good place to handle sending things to Agora/any actions that happen in the release process after tests succeed
 # Note: script will run after xcodebuild command even if the xcodebuild command fails
+LIST_ENDPOINT="https://agora.dequecloud.com/artifactory/api/storage/Attest-iOS/axeDevToolsXCUI/frameworks"
+
+# List all files in beta directory
+LIST_RESPONSE=$(curl -s -X GET "$LIST_ENDPOINT" -H "X-JFrog-Art-Api: $DQ_AGORA_KEY")
+echo=$LIST_RESPONSE
+# Check if the response contains any files
+FILE_COUNT=$(echo "$LIST_RESPONSE" | jq '.files | length')
+echo=$FILE_COUNT
+done
