72edce5cd685b7b1c1ce521634de663e99698fa4
Kate Owens <kate.owens@deque.com>
2025-07-09 15:33:26 -0400

    Let's see what happens when we run a test plan

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -4,13 +4,12 @@
 #  axe-devtools-ios-sample-app
 #
 #  Created by Kate Owens on 7/8/25.
-#  
-
-# This would be a good place to handle sending things to Agora/any actions that happen in the release process after tests succeed
+#
 # Note: script will run after xcodebuild command even if the xcodebuild command fails
+
 LIST_ENDPOINT="https://agora.dequecloud.com/artifactory/api/storage/Attest-iOS/axeDevToolsXCUI/frameworks"
 
-# List all files in beta directory
+# List all files in framework directory
 LIST_RESPONSE=$(curl -s -X GET "$LIST_ENDPOINT" -H "X-JFrog-Art-Api: $DQ_AGORA_KEY")
 echo "$LIST_RESPONSE"
 # Check if the response contains any files
