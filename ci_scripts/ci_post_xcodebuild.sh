c46c8918abb6d8bd3fc0d7e4d2a8096f61ce5b6b
Kate Owens <kate.owens@deque.com>
2025-07-10 12:32:55 -0400

    in the wrong directory?

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -11,7 +11,8 @@ set -e
 #cd "$CI_WORKSPACE" || exit 1
 echo "📂 Current working directory: $(pwd)"
 ls -la
-
+cd ..
+echo "now we are in $(pwd)"
 PLIST_PATH=$(find . -name Info.plist) #"axe-devtools-ios-sample-app/axe-devtools-ios-sample-app/Info.plist"
 
 if [ ! -f "$PLIST_PATH" ]; then
