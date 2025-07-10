d709740afb72883ec556ef6df884ce996e810cff
Kate Owens <kate.owens@deque.com>
2025-07-10 15:22:54 -0400

    update files

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -6,19 +6,6 @@
 #  Created by Kate Owens on 7/8/25.
 #  
 
-set -e
-
-echo "✅ Pre-Xcodebuild Script Starting"
-
-cd "$CI_WORKSPACE" || exit 1
-
-brew install gh
-gh auth status
-
-
-#cd "$CI_WORKSPACE" || exit 1
-echo "📂 Current working directory: $(pwd)"
-ls -la
-
-PLIST_PATH=$(find . -name Info.plist) #"axe-devtools-ios-sample-app/axe-devtools-ios-sample-app/Info.plist"
-echo "$(PLIST_PATH)"
+# Examples of what can go here
+# Installing GH CLI tool via homebrew
+# 
