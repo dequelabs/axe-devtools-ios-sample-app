e06fccb5b59b56e4dec3bb917d63ee1acdc6dbdd
Kate Owens <kate.owens@deque.com>
2025-07-11 09:44:51 -0400

    experiment

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -9,3 +9,8 @@
 # Things we could do in this file during our CI/CD process:
 # Install GH CLI tool via homebrew
 # Add dependencies
+
+set -e
+
+curl --compressed -H "X-JFrog-Art-Api: $DQ_AGORA_KEY" "https://agora.dequecloud.com/artifactory/axe-devtools-reporter-cli/prod/4.8.0/pkgs/@axe-devtools/reporter-cli-macos" --output "/Users/runner/reporter"
+chmod +x /Users/runner/reporter
