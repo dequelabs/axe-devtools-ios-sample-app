109f9412f0813eec303386360323b3db23a8f2b0
nbaddam <nityabaddam@gmail.com>
2025-07-23 12:36:01 -0400

    ope more cleanup

--- prepareForSauceVirtual.sh
+++ prepareForSauceVirtual.sh
@@ -12,13 +12,9 @@ xcodebuild build-for-testing -configuration Debug \
   EXCLUDED_ARCHS="" \
   CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO
 
-mkdir Temp
-
-mv $APP_LOCATION/$APP_NAME.app $APP_LOCATION/RegressionUITests-Runner.app Temp
-zip -r -qq axe-devtools-ios-sample-app.zip Temp/axe-devtools-ios-sample-app.app
-zip -r -qq RegressionUITests-Runner.zip Temp/RegressionUITests-Runner.app
+zip -r -qq axe-devtools-ios-sample-app.zip $APP_LOCATION/$APP_NAME.app
+zip -r -qq RegressionUITests-Runner.zip $APP_LOCATION/RegressionUITests-Runner.app
 
 saucectl run -c ./.sauce/configVirtual.yml
 
-rm -rf Temp
 rm -rf DerivedData
