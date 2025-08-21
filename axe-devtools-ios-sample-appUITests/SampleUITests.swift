22772fb670f738520d54481897505c41fc8a93b3
Kyle Lee <kylelee@Kyles-MacBook-Pro.local>
2025-08-21 13:08:51 -0700

    Ignore experimental and update reporter

--- axe-devtools-ios-sample-appUITests/SampleUITests.swift
+++ axe-devtools-ios-sample-appUITests/SampleUITests.swift
@@ -99,6 +99,11 @@ class SampleUITests: XCTestCase {
     // For more information on saving results locally, please visit:
     // https://docs.deque.com/devtools-mobile/2023.8.16/en/ios-save-result
     func testAccessibilityAndSaveResultsLocally() throws {
+
+        // Experimental rules are rulesets that are still in testing and development. Results for the experimental
+        // rules can be IGNORED with the ignoreExperimental method, and this way they will not run
+        axe?.configuration.ignoreExperimental()
+
         var lastResult: AxeResult?
         guard let result = try axe?.run(onElement: app) else {
             XCTFail("\n\n🦮 axe DevTools didn't run - Did you add your API key in Login.swift?\n\n")
